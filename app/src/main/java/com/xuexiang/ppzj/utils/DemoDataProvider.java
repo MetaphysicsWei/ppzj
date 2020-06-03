ceaa3c034e4a836e79a2fd51fab1fa892cd02698
Zzgj <52107195+Zzgj@users.noreply.github.com>
2020-06-03 17:06:09 +0800

    3
    修改资讯列表内容

--- app/src/main/java/com/xuexiang/ppzj/utils/DemoDataProvider.java
+++ app/src/main/java/com/xuexiang/ppzj/utils/DemoDataProvider.java
@@ -78,30 +78,47 @@ public class DemoDataProvider {
     @MemoryCache
     public static List<NewInfo> getDemoNewInfos() {
         List<NewInfo> list = new ArrayList<>();
-        list.add(new NewInfo("源码", "良渚凭什么申遗成功？一眼5000年的古城，是一生必去的打卡地！")
-                .setSummary("良渚凭什么申遗成功？一眼5000年的古城，是一生必去的打卡地！")
+        list.add(new NewInfo("资讯", "良渚凭什么申遗成功？一眼5000年的古城，是一生必去的打卡地！")
+                .setSummary("浙江杭州良渚古城遗址，被正式列入《世界遗产名录》，中国以55处世界遗产的数量，位列世界遗产总数第一！")
                 .setDetailUrl("http://www.tourzj.gov.cn/play/ddetails_zixun.aspx?id=1864")
                 .setImageUrl("http://img.tourzj.com/admin/media/eecfd82f-74b4-4311-a023-e805d63f4172.jpg"));
-        list.add(new NewInfo("Android UI", "XUI 一个简洁而优雅的Android原生UI框架，解放你的双手")
-                .setSummary("涵盖绝大部分的UI组件：TextView、Button、EditText、ImageView、Spinner、Picker、Dialog、PopupWindow、ProgressBar、LoadingView、StateLayout、FlowLayout、Switch、Actionbar、TabBar、Banner、GuideView、BadgeView、MarqueeView、WebView、SearchView等一系列的组件和丰富多彩的样式主题。\n")
-                .setDetailUrl("https://juejin.im/post/5c3ed1dae51d4543805ea48d")
-                .setImageUrl("https://user-gold-cdn.xitu.io/2019/1/16/1685563ae5456408?imageView2/0/w/1280/h/960/format/webp/ignore-error/1"));
+        list.add(new NewInfo("资讯", "别再惦记蓬莱仙境，去浙8个隐世禅修地当一回“无忧人”！")
+                .setSummary("终日困在“钢筋森林”中的我们，被现实生活中的各类烦恼所扰。为何不寻一处清净之地，来一场修身养性的禅修之旅！")
+                .setDetailUrl("http://www.tourzj.gov.cn/play/ddetails_zixun.aspx?id=1865")
+                .setImageUrl("http://img.tourzj.com/admin/media/09064528-1ddd-4987-99e5-4718663f5935.jpg"));
 
-        list.add(new NewInfo("面试", "写给即将面试的你")
-                .setSummary("最近由于公司业务发展，需要招聘技术方面的人才，由于我在技术方面比较熟悉，技术面的任务就交给我了。今天我要分享的就和面试有关，主要包含技术面的流程、经验和建议，避免大家在今后的面试过程中走一些弯路，帮助即将需要跳槽面试的人。")
-                .setDetailUrl("https://juejin.im/post/5ca4df966fb9a05e4e58320c")
-                .setImageUrl("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1554629219186&di=6cdab5cfceaae1f7e6d78dbe79104c9f&imgtype=0&src=http%3A%2F%2Fimg.qinxue365.com%2Fuploads%2Fallimg%2F1902%2F4158-1Z22FZ64E00.jpg"));
+        list.add(new NewInfo("资讯", "连非洲人都热回家的夏天，你能去哪儿？答案在这里……")
+                .setSummary("到哪里才能逃离这个闷热的城市，找到一片cool到没边的清凉之地？")
+                .setDetailUrl("http://www.tourzj.gov.cn/play/ddetails_zixun.aspx?id=1867")
+                .setImageUrl("http://img.tourzj.com/admin/media/6ecb0a21-a457-432f-8468-60f6635cd19f.jpg"));
 
-        list.add(new NewInfo("Android", "XUpdate 一个轻量级、高可用性的Android版本更新框架")
-                .setSummary("XUpdate 一个轻量级、高可用性的Android版本更新框架。本框架借鉴了AppUpdate中的部分思想和UI界面，将版本更新中的各部分环节抽离出来，形成了如下几个部分：")
-                .setDetailUrl("https://juejin.im/post/5b480b79e51d45190905ef44")
-                .setImageUrl("https://user-gold-cdn.xitu.io/2018/7/13/16492d9b7877dc21?imageView2/0/w/1280/h/960/format/webp/ignore-error/1"));
+        list.add(new NewInfo("资讯", "刷遍夏日美景！浙江十大最美自驾公路，周末带上全家去兜风！")
+                .setSummary("这条隐藏在新疆深处的“奇迹之路，其瑰伟不亚于川藏线")
+                .setDetailUrl("http://www.tourzj.gov.cn/play/ddetails_zixun.aspx?id=1869")
+                .setImageUrl("http://img.tourzj.com/admin/media/61d274cc-bc70-4b3d-9fbc-5db8d71503bc.jpg"));
 
-        list.add(new NewInfo("Android/HTTP", "XHttp2 一个功能强悍的网络请求库，使用RxJava2 + Retrofit2 + OKHttp进行组装")
-                .setSummary("一个功能强悍的网络请求库，使用RxJava2 + Retrofit2 + OKHttp组合进行封装。还不赶紧点击使用说明文档，体验一下吧！")
-                .setDetailUrl("https://juejin.im/post/5b6b9b49e51d4576b828978d")
-                .setImageUrl("https://user-gold-cdn.xitu.io/2018/8/9/1651c568a7e30e02?imageView2/0/w/1280/h/960/format/webp/ignore-error/1"));
+        list.add(new NewInfo("资讯", "高温席卷的夏天，不如去这些堪比童话仙境的小众森林里乘个凉！")
+                .setSummary("逶迤起伏的群山，云雾飘渺的天空。奔流而下的溪涧，凉爽清新的山风。")
+                .setDetailUrl("http://www.tourzj.gov.cn/play/ddetails_zixun.aspx?id=1870")
+                .setImageUrl("http://img.tourzj.com/admin/media/c284e0a2-2ea0-4528-91b5-d713e1fa6f4e.jpg"));
+
+        list.add(new NewInfo("资讯", "洗肺又养眼！浙7条徒步道带你领略溢出屏幕的清新！")
+                .setSummary("趁着天气还没有真正热起来，不如到山里运动一下，一起去看山花，挖野菜，轻松溜达，享受山海美景！")
+                .setDetailUrl("http://www.tourzj.gov.cn/play/ddetails_zixun.aspx?id=1876")
+                .setImageUrl("http://img.tourzj.com/admin/media/e467a122-4734-4ad4-b58a-3eee7ede3efa.jpg"));
+
+        list.add(new NewInfo("资讯", "抢头条了！德云社烧饼、摇滚巨匠汪峰&黄贯中、灰姑娘…你最期待谁？")
+                .setSummary("如果你想找笑点，浙里有德云社艺人为你讲相声；如果你想听天籁，浙里有来自巴黎圣母院的男童合唱团；如果你想嗨翻天，浙里有摇滚巨匠汪峰对战黄贯中。")
+                .setDetailUrl("http://www.tourzj.gov.cn/play/ddetails_zixun.aspx?id=1877")
+                .setImageUrl("http://img.tourzj.com/admin/media/a56dfb64-a7a6-4c1f-84a3-406ace18e28c.jpg"));
+
+        list.add(new NewInfo("资讯", "浙6个深山隐居地，才是夏天最值得一去的旅行地！")
+                .setSummary("一年中最清新的夏季时节又将到来 挑个日子，拣个山头，树下闲坐，看风吹起")
+                .setDetailUrl("http://www.tourzj.gov.cn/play/ddetails_zixun.aspx?id=1883")
+                .setImageUrl("http://img.tourzj.com/admin/media/2c8ea702-d55b-4c46-8182-0493e63f77f5.jpg"));
         return list;
+
+
     }
 
     public static List<AdapterItem> getGridItems(Context context) {
