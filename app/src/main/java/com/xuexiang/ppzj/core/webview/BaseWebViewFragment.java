89a660b91687235bd1c11b0bef7e250f88c595ba
MetaphysicsWei <51878782+MetaphysicsWei@users.noreply.github.com>
2020-06-12 14:28:01 +0800

    17.1

--- app/src/main/java/com/xuexiang/ppzj/core/webview/BaseWebViewFragment.java
+++ app/src/main/java/com/xuexiang/ppzj/core/webview/BaseWebViewFragment.java
@@ -1,20 +1,3 @@
-/*
- * Copyright (C) 2019 xuexiangjys(xuexiangjys@163.com)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
 package com.xuexiang.ppzj.core.webview;
 
 import android.view.KeyEvent;
@@ -22,12 +5,6 @@ import android.view.KeyEvent;
 import com.just.agentweb.core.AgentWeb;
 import com.xuexiang.ppzj.core.BaseFragment;
 
-/**
- * 基础web
- *
- * @author xuexiang
- * @since 2019/5/28 10:22
- */
 public abstract class BaseWebViewFragment extends BaseFragment {
 
     protected AgentWeb mAgentWeb;
